1f247d227ee19725d1fdb96355bfd992c9d7a7ef
fyfh <13757260730@sina.cn>
2020-09-17 15:06:34 +0800

    Add files via upload

--- tcp_nanqinlang.c
+++ tcp_nanqinlang.c
@@ -1,9 +1,7 @@
 /* tcp_nanqinlang
- 
- * edit:https://github.com/tcp-nanqinlang/generation-2/blob/master/tcp_nanqinlang2.c
- 
+
  * Debian
- 
+
  * general kernel
 
  × New BBR Congestion Control
@@ -145,59 +143,59 @@ struct bbr {
 
 
 /* Window length of bw filter (in rounds): */
-static const int bbr_bw_rtts = CYCLE_LEN + 7;
+static const int bbr2_bw_rtts = CYCLE_LEN + 2;
 /* Window length of min_rtt filter (in sec): */
-static const u32 bbr_min_rtt_win_sec = 5;
-/* Minimum time (in ms) spent at bbr_cwnd_min_target in BBR_PROBE_RTT mode: */
-static const u32 bbr_probe_rtt_mode_ms = 100;
+static const u32 bbr2_min_rtt_win_sec = 10;
+/* Minimum time (in ms) spent at bbr2_cwnd_min_target in BBR2_PROBE_RTT mode: */
+static const u32 bbr2_probe_rtt_mode_ms = 200;
 /* Skip TSO below the following bandwidth (bits/sec): */
-static const int bbr_min_tso_rate = 1024000;
+static const int bbr2_min_tso_rate = 1200000;
 
 /* We use a high_gain value of 2/ln(2) because it's the smallest pacing gain
  * that will allow a smoothly increasing pacing rate that will double each RTT
  * and send the same number of packets per RTT that an un-paced, slow-starting
  * Reno or CUBIC flow would:
  */
-static const int bbr_high_gain  = BBR_UNIT * 3000 / 1000 + 1;
-/* The pacing gain of 1/high_gain in BBR_DRAIN is calculated to typically drain
- * the queue created in BBR_STARTUP in a single round:
+static const int bbr2_high_gain  = BBR2_UNIT * 2885 / 1000 + 1;
+/* The pacing gain of 1/high_gain in BBR2_DRAIN is calculated to typically drain
+ * the queue created in BBR2_STARTUP in a single round:
  */
-static const int bbr_drain_gain = BBR_UNIT *  800 / 2800;
+static const int bbr2_drain_gain = BBR2_UNIT * 1000 / 2885;
 /* The gain for deriving steady-state cwnd tolerates delayed/stretched ACKs: */
-static const int bbr_cwnd_gain  = BBR_UNIT * 2;
+static const int bbr2_cwnd_gain  = BBR2_UNIT * 2;
 /* The pacing_gain values for the PROBE_BW gain cycle, to discover/share bw: */
-static const int bbr_pacing_gain[] = {
-	BBR_UNIT * 6 / 4,	/* probe for more available bw */
-	BBR_UNIT * 3 / 4,	/* drain queue and/or yield bw to other flows */
-	BBR_UNIT * 5 / 4, BBR_UNIT * 5 / 4, BBR_UNIT * 5 / 4,	/* cruise at 1.0*bw to utilize pipe, */
-	BBR_UNIT * 5 / 4, BBR_UNIT * 5 / 4, BBR_UNIT * 5 / 4	/* without creating excess queue... */
+static const int bbr2_pacing_gain[] = {
+	BBR2_UNIT * 5 / 4,	/* probe for more available bw */
+	BBR2_UNIT * 3 / 4,	/* drain queue and/or yield bw to other flows */
+	BBR2_UNIT, BBR2_UNIT, BBR2_UNIT,	/* cruise at 1.0*bw to utilize pipe, */
+	BBR2_UNIT, BBR2_UNIT, BBR2_UNIT	/* without creating excess queue... */
 };
 /* Randomize the starting gain cycling phase over N phases: */
-static const u32 bbr_cycle_rand = 7;
+static const u32 bbr2_cycle_rand = 7;
 
 /* Try to keep at least this many packets in flight, if things go smoothly. For
  * smooth functioning, a sliding window protocol ACKing every other packet
  * needs at least 4 packets in flight:
  */
-static const u32 bbr_cwnd_min_target = 4;
+static const u32 bbr2_cwnd_min_target = 4;
 
-/* To estimate if BBR_STARTUP mode (i.e. high_gain) has filled pipe... */
+/* To estimate if BBR2_STARTUP mode (i.e. high_gain) has filled pipe... */
 /* If bw has increased significantly (1.25x), there may be more bw available: */
-static const u32 bbr_full_bw_thresh = BBR_UNIT * 6 / 4;
+static const u32 bbr2_full_bw_thresh = BBR2_UNIT * 5 / 4;
 /* But after 3 rounds w/o significant bw growth, estimate pipe is full: */
-static const u32 bbr_full_bw_cnt = 3;
+static const u32 bbr2_full_bw_cnt = 3;
 
 /* "long-term" ("LT") bandwidth estimator parameters... */
 /* The minimum number of rounds in an LT bw sampling interval: */
-static const u32 bbr_lt_intvl_min_rtts = 4;
+static const u32 bbr2_lt_intvl_min_rtts = 4;
 /* If lost/delivered ratio > 20%, interval is "lossy" and we may be policed: */
-static const u32 bbr_lt_loss_thresh = 50;
+static const u32 bbr2_lt_loss_thresh = 50;
 /* If 2 intervals have a bw ratio <= 1/8, their bw is "consistent": */
-static const u32 bbr_lt_bw_ratio = BBR_UNIT / 8;
+static const u32 bbr2_lt_bw_ratio = BBR2_UNIT / 8;
 /* If 2 intervals have a bw diff <= 4 Kbit/sec their bw is "consistent": */
-static const u32 bbr_lt_bw_diff = 4000 / 8;
+static const u32 bbr2_lt_bw_diff = 4000 / 8;
 /* If we estimate we're policed, use lt_bw for this many round trips: */
-static const u32 bbr_lt_bw_max_rtts = 40;
+static const u32 bbr2_lt_bw_max_rtts = 48;
 
 /* Do we estimate that STARTUP filled the pipe? */
 static bool bbr_full_bw_reached(const struct sock *sk)
